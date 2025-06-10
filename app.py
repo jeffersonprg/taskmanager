f69c1f351694df30b7593cfca3e01f00e1e064eb
Jefferson <jeffersonprg@gmail.com>
2025-06-10 16:31:20 +0100

    projeto task manager finalizado

--- app.py
+++ app.py
@@ -1,44 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request,  redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)  # Servidor web Flask
+app = Flask(__name__)
+app.secret_key = "1234"  # Chave secreta para proteger sessões e cookies
 
-# Caminho correto do banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tarefas.db"
+# Corrigido: banco de dados na mesma pasta do app
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:/Users/Gebruiker/PycharmProjects/GestordeTarefas1/database/tarefas.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)  # Inicializa a conexão com o banco de dados
+db = SQLAlchemy(app)
 
-# Definição do modelo da tabela de tarefas
-# Cada tarefa tem um ID, conteúdo e status de concluída
 class Tarefa(db.Model):
     __tablename__ = "tarefas"
-
     id = db.Column(db.Integer, primary_key=True)
     conteudo = db.Column(db.String(200), nullable=False)
     feita = db.Column(db.Boolean, default=False)
-
 
 with app.app_context():
     db.create_all()
     db.session.commit()
 
-# Página principal
 @app.route('/')
 def home():
-    return render_template("index.html")
+    todas_as_tarefas = Tarefa.query.all()
+    return render_template("index.html", lista_de_tarefas=todas_as_tarefas)
 
-# Rota para criar tarefa
-@app.route('/criar', methods=['POST'])
+@app.route('/criar-tarefa', methods=['POST'])
 def criar():
-    conteudo = request.form.get('conteudo_tarefa')
-    if not conteudo:
-        return 'Conteúdo inválido', 400
+    try:
+        conteudo_tarefa = request.form['conteudo_tarefa']
+    except KeyError:
+        return "Erro: chave 'conteudo_tarefa' não encontrada no formulário.", 400
 
-    nova_tarefa = Tarefa(conteudo=conteudo, feita=False)
-    db.session.add(nova_tarefa)
+    tarefa = Tarefa(conteudo=conteudo_tarefa, feita=False)
+    db.session.add(tarefa)
+    db.session.commit()
+    return redirect(url_for('home')) # Redireciona-nos à função home()
+@app.route('/eliminar-tarefa/<id>')
+def eliminar(id):
+    tarefa = Tarefa.query.filter_by(id=int(id)).delete() # Pesquisa-se dentro da base de dados, aquele registro cujo id coincida com o proporcionado peloparâmetro da rota. Quando se encontrar elimina-se
+    db.session.commit() # Executar a operação pendente da base de dados
+    return redirect(url_for('home'))
+@app.route('/tarefa-feita/<id>')
+def tarefa_feita(id):
+    tarefa = Tarefa.query.filter_by(id=int(id)).first()
+    tarefa.feita = not(tarefa.feita)
     db.session.commit()
     return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
